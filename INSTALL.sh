63cf105acadef09abc12f63f880239579a07c02c
Diego García Cosío <garco97@MacBook-Pro-de-Diego.local>
2025-01-18 19:54:08 +0100

    Updated INSTALL.sh script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 ln  .vimrc ~/
 ln  .zshrc ~/
+ln  .tmux.conf ~/
 
 git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
