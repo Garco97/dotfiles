f615dc89916b0957e9b1254552a417292a7b2549
Diego García Cosío <dgarciacosio@albentia.com>
2025-11-29 13:25:23 +0100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOTFILES="$HOME/.dotfiles"
+
+# Zsh
+ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+
+# Starship
+mkdir -p ~/.config
+ln -sf "$DOTFILES/starship/starship.toml" "$HOME/.config/starship.toml"
+
+# Vim
+ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
+
+# Neovim
+mkdir -p ~/.config
+ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
+
+# Tmux
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+
